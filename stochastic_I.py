232c670732c35dd4a2a739ccd3b1afc9497ca52d
jaberkow <jackberkowitz88@gmail.com>
2018-06-08 11:28:22 -0700

    added the nonlinear IB upper bound

--- stochastic_I.py
+++ stochastic_I.py
@@ -2,6 +2,28 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.distributions import Logistic,RelaxedBernoulli,BernoulliWithSigmoidProbs
 
+def I_ub(x):
+    """
+    Computes the non-parametric upper bound on mutual information derived in (https://arxiv.org/abs/1705.02436),
+    for the case of binary neurons.
+    
+    Inputs:
+        x:  Real valued tensor of shape [batch size,number of neurons], is the "activation" 
+            of each neuron for each sample  We assume that the activation is separate 
+    Outputs:
+        I:  Real valued scalar.  Upper bound on the information (in nats).
+    """
+    
+    
+    A_op = tf.reduce_sum(A_tf(x),1)
+    m_bar_op = tf.tanh(x)
+    A_diff_op = tf.expand_dims(A_op,1) - tf.expand_dims(A_op,0)
+    x_diff_op = tf.expand_dims(x,1) - tf.expand_dims(x,0)
+    m_temp = tf.tile(tf.expand_dims(m_bar_op,1),[1,batch_size,1])
+    KL_op = tf.reduce_sum(m_temp*x_diff_op,2) - A_diff_op
+    I_op = -tf.reduce_mean(tf.log(tf.reduce_mean(tf.exp(-KL_op),1)))
+    return I_op
+
 def A_tf(f):
     """
     Computes the log partition function A
@@ -25,7 +47,7 @@ def I_hat(f,temperature,samples_per_x):
             strictly greater than 0.
         samples_per_x:  A 0-D tensor of type int32 or int64.  How many samples of \vec{r} to draw per sample of x.
     Outputs:
-        I:  Real valued scalar.  Stochastic estimate of the mutual information.
+        I:  Real valued scalar.  Stochastic estimate of the mutual information (in nats).
         r_samples:  Tensor of shape [samples_per_x*batch_size,number_of_neurons]. r_samples[i*samples_per_x:(i+1)*samples_per_x,j] are
             samples for the jth neuron for sample i.
     """
